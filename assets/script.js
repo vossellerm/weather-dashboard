61cf9a51aafcdf574382253c513d5ed1f7b37bdb
Mikayla Vosseller <vossellerm@gmail.com>
2022-04-11 22:05:44 -0700

    forecast autopopulates

--- assets/script.js
+++ assets/script.js
@@ -104,7 +104,7 @@ var formSubmitHandler = function (event) {
           latitude +
           "&lon=" +
           longitude +
-          "&exclude=hourly,daily" +
+          "&exclude=hourly" +
           "&appid=" +
           APIKey +
           "&units=imperial";
@@ -115,6 +115,7 @@ var formSubmitHandler = function (event) {
           .then(function (data) {
             console.log(data);
 
+            // current Day
             var city = document.getElementById("cityName");
             var date = new Date();
             var currentDate = document.getElementById("currentDate");
@@ -122,6 +123,7 @@ var formSubmitHandler = function (event) {
             var currentWind = document.getElementById("currentWind");
             var currentHumidity = document.getElementById("currentHumidity");
             var uv = document.getElementById("uv");
+
             city.textContent = cityText;
             currentDate.textContent = moment(
               date,
@@ -131,6 +133,71 @@ var formSubmitHandler = function (event) {
             currentWind.textContent = data.current.wind_speed;
             currentHumidity.textContent = data.current.humidity;
             uv.textContent = data.current.uvi;
+
+            // day 1
+            var date1 = document.getElementById("date1");
+            var temp1 = document.getElementById("temp1");
+            var wind1 = document.getElementById("wind1");
+            var humidity1 = document.getElementById("humidity1");
+
+            date1.textContent = moment(data.daily[1].dt, "X").format(
+              "M/D/YYYY"
+            );
+            temp1.textContent = data.daily[1].temp.day;
+            wind1.textContent = data.daily[1].wind_speed;
+            humidity1.textContent = data.daily[1].humidity;
+
+            // day 2
+            var date2 = document.getElementById("date2");
+            var temp2 = document.getElementById("temp2");
+            var wind2 = document.getElementById("wind2");
+            var humidity2 = document.getElementById("humidity2");
+
+            date2.textContent = moment(data.daily[2].dt, "X").format(
+              "M/D/YYYY"
+            );
+            temp2.textContent = data.daily[2].temp.day;
+            wind2.textContent = data.daily[2].wind_speed;
+            humidity2.textContent = data.daily[2].humidity;
+
+            // day 3
+            var date3 = document.getElementById("date3");
+            var temp3 = document.getElementById("temp3");
+            var wind3 = document.getElementById("wind3");
+            var humidity3 = document.getElementById("humidity3");
+
+            date3.textContent = moment(data.daily[3].dt, "X").format(
+              "M/D/YYYY"
+            );
+            temp3.textContent = data.daily[3].temp.day;
+            wind3.textContent = data.daily[3].wind_speed;
+            humidity3.textContent = data.daily[3].humidity;
+
+            // day 4
+            var date4 = document.getElementById("date4");
+            var temp4 = document.getElementById("temp4");
+            var wind4 = document.getElementById("wind4");
+            var humidity4 = document.getElementById("humidity4");
+
+            date4.textContent = moment(data.daily[4].dt, "X").format(
+              "M/D/YYYY"
+            );
+            temp4.textContent = data.daily[4].temp.day;
+            wind4.textContent = data.daily[4].wind_speed;
+            humidity4.textContent = data.daily[4].humidity;
+
+            // day 5
+            var date5 = document.getElementById("date5");
+            var temp5 = document.getElementById("temp5");
+            var wind5 = document.getElementById("wind5");
+            var humidity5 = document.getElementById("humidity5");
+
+            date5.textContent = moment(data.daily[4].dt, "X").format(
+              "M/D/YYYY"
+            );
+            temp5.textContent = data.daily[4].temp.day;
+            wind5.textContent = data.daily[4].wind_speed;
+            humidity5.textContent = data.daily[4].humidity;
           });
       });
   } else {
