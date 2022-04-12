df1b14eb58796cd6ccafa8ba1af9e48b0a366520
Mikayla Vosseller <vossellerm@gmail.com>
2022-04-11 18:44:13 -0700

    text input added to local storage

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,51 @@
-// API key from weather API
-var APIKey = "901c73df804f3ef17710b144f76aa6c2";
+// // API key from weather API
+// var APIKey = "901c73df804f3ef17710b144f76aa6c2";
 
-// variable to store city name collected from weather API
-var city = "San Diego";
+// // variable to store city name collected from weather API
+// var city = "San Diego";
 
-// variable to store current weather data URL and necessary variables
-var queryURL =
-  "http://api.openweathermap.org/data/2.5/weather?q=" +
-  city +
-  "&appid=" +
-  APIKey;
+// // variable to store current weather data URL and necessary variables
+// var queryURL =
+//   "http://api.openweathermap.org/data/2.5/weather?q=" +
+//   city +
+//   "&appid=" +
+//   APIKey;
 
-// fetch API
-fetch(queryURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+// // fetch API
+// fetch(queryURL)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   });
+
+var cityInputEl = document.querySelector("#city");
+var weatherFormEl = document.querySelector("#weather-form");
+
+var cities = [];
+
+function storeCities() {
+  localStorage.setItem("cities", JSON.stringify(cities));
+}
+
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+
+  var city = cityInputEl.value.trim();
+
+  if (city) {
+    cityInputEl.value = "";
+  } else {
+    alert("Please enter a city");
+  }
+
+  cities.push(city);
+
+  storeCities();
+};
+
+weatherFormEl.addEventListener("submit", formSubmitHandler);
 
 var searchBtn = document.getElementById("searchBtn");
 var weather = document.getElementById("weather");
